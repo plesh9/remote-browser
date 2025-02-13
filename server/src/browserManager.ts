bc3f414138754f43c36b29e1ee82d7ae3e1f7bf9
Kashchenko Vadym <game.plesh9@gmail.com>
2025-02-13 17:34:14 +0200

    show cp

--- server/src/browserManager.ts
+++ server/src/browserManager.ts
@@ -1,18 +1,24 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 import { WebSocket } from "ws";
 
-const WINDOW_SIZE = { width: 375, height: 667 };
-
 export class BrowserManager {
   private browser: Browser | null = null;
   private page: Page | null = null;
 
-  async launchBrowser(url: string) {
+  async launchBrowser({
+    url,
+    deviceWidth,
+    deviceHeight,
+  }: {
+    url: string;
+    deviceWidth: number;
+    deviceHeight: number;
+  }) {
     this.browser = await puppeteer.launch({
       headless: false,
       args: [
         `
-        --window-size=;${WINDOW_SIZE.width},${WINDOW_SIZE.height}`,
+        --window-size=;${deviceWidth},${deviceHeight}`,
         "--disable-infobars",
         "--mute-audio",
         "--disable-features=TranslateUI",
@@ -20,8 +26,8 @@ export class BrowserManager {
     });
     this.page = await this.browser.newPage();
     await this.page.setViewport({
-      width: WINDOW_SIZE.width,
-      height: WINDOW_SIZE.height,
+      width: deviceWidth,
+      height: deviceHeight,
       deviceScaleFactor: 2,
     });
     await this.page.goto(url, { waitUntil: "domcontentloaded" });
@@ -44,6 +50,9 @@ export class BrowserManager {
             html {
               overflow: hidden !important;
             }
+            .login_content {
+              background: none !important;
+            }
             .login-cols {
               display: flex !important;
               justify-content: center !important;
@@ -52,13 +61,9 @@ export class BrowserManager {
               overflow: hidden !important;
               padding: 16px !important;
             }
-            // .v-text-field__details {
-            //   display: none !important;
-            // }
-            // :root {
-            //   --bg-color: #242529;
-            //   --text-color: #fff;
-            // }
+            .login-cols > .login-col:first-child {
+              display: none !important;
+            }
           `
         )
       );
@@ -81,15 +86,99 @@ export class BrowserManager {
         ?.remove();
       document.querySelector(".b-login-posts-outer")?.remove();
       document.querySelector(".l-footer-static")?.remove();
+      document.querySelector(".contact_button")?.remove();
     });
+
+    const selector = ".captcha_wrapper";
+
+    await this.page.evaluate((selector) => {
+      return new Promise<void>((resolve) => {
+        const observer = new MutationObserver((mutations, obs) => {
+          if (document.querySelector(selector)) {
+            obs.disconnect();
+            setTimeout(resolve, 2500);
+          }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          setTimeout(resolve, 2500);
+        }
+      });
+    }, selector);
+
+    const boundingBox = await this.page.evaluate((selector) => {
+      const element = document.querySelector(selector);
+
+      if (!element) {
+        return null;
+      }
+
+      const rect = element.getBoundingClientRect();
+
+      return {
+        x: rect.left + rect.width / 2 + window.scrollX,
+        y: rect.top + rect.height / 2 + window.scrollY,
+      };
+    }, selector);
+
+    if (!boundingBox) {
+      return;
+    }
+
+    await this.page.mouse.click(boundingBox.x, boundingBox.y);
   }
 
   async getScreenshot() {
     if (this.page) {
-      return await this.page!.screenshot({
+      const captchaModalSelector = 'iframe[src*="recaptcha/enterprise/bframe"]';
+
+      const iframeElement = await this.page.$(captchaModalSelector);
+      if (!iframeElement) {
+        console.log("reCAPTCHA iframe не знайдено.");
+        return null;
+      }
+
+      const isVisible = await this.page.evaluate((selector) => {
+        const iframe = document.querySelector(selector);
+        if (!iframe) return false;
+
+        const parentCaptchaModal = iframe.parentElement?.parentElement;
+        if (!parentCaptchaModal) return false;
+
+        const style = window.getComputedStyle(parentCaptchaModal);
+        return style.display !== "none" && style.visibility !== "hidden";
+      }, captchaModalSelector);
+
+      if (!isVisible) {
+        console.log("Капча прихована.");
+        return null;
+      }
+
+      const boundingBox = await iframeElement.boundingBox();
+      if (!boundingBox) {
+        console.log("Не вдалося отримати розміри iframe.");
+        return null;
+      }
+
+      const screenshotBuffer = await this.page.screenshot({
         encoding: "binary",
         type: "png",
-      });
+        clip: {
+          x: boundingBox.x,
+          y: boundingBox.y,
+          width: boundingBox.width,
+          height: boundingBox.height,
+        },
+      })
+
+      return {
+        screenshotBuffer,
+        screenshotWidth: boundingBox.width,
+        screenshotHeight: boundingBox.height,
+      };
     }
 
     return null;
@@ -100,7 +189,40 @@ export class BrowserManager {
       const { eventType, x, y } = event;
 
       if (eventType === "click") {
-        await this.page.mouse.click(x, y);
+        const captchaModalSelector =
+          'iframe[src*="recaptcha/enterprise/bframe"]';
+
+        const iframeElement = await this.page.$(captchaModalSelector);
+        if (!iframeElement) {
+          console.log("Iframe не знайдено.");
+          return;
+        }
+
+        const frame = await iframeElement.contentFrame();
+        if (!frame) {
+          console.log("Не вдалося отримати вміст iframe.");
+          return;
+        }
+
+        const boundingBox = await iframeElement.boundingBox();
+        if (!boundingBox) {
+          console.log("Не вдалося отримати розміри iframe.");
+          return;
+        }
+
+        await frame.evaluate(
+          ({ x, y }) => {
+            const clickEvent = new MouseEvent("click", {
+              bubbles: true,
+              cancelable: true,
+              clientX: x,
+              clientY: y,
+            });
+
+            document.elementFromPoint(x, y)?.dispatchEvent(clickEvent);
+          },
+          { x, y }
+        );
       }
     }
   }
